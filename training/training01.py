5f9eaf9939144836d1cda660adadb0e6106c3f91
KristinaKalab <kris48@yandex.ru>
2024-09-08 19:17:07 +0300

    new file:   training/training01.py

---
+++ training/training01.py
@@ -0,0 +1,10 @@
+my_heigh=165
+print (my_heigh)
+
+my_name = "Kristina"
+my_name ="Kristina Kalaburdina"
+
+print (my_name)
+
+pet_name = input("Как зовут вашего питомца? ")
+print ("Ваш любимчик - " + pet_name)
