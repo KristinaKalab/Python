9761fb79fa0ea30ce91ddab814408bd21281ea54
KristinaKalab <kris48@yandex.ru>
2024-10-05 12:13:56 +0300

    git add -a

---
+++ training_02/list8.py
@@ -0,0 +1,9 @@
+var_1 = 50
+var_2 = 5
+print("Значение var_1:", var_1)
+print("Значение var_2:", var_2)
+
+var_1, var_2 = var_2, var_1
+
+print("Обновленное значение var_1:", var_1)
+print("Обновленное значение var_2:", var_2)
---
+++ training_03/book.py
@@ -0,0 +1,4 @@
+class Book:
+    def __init__(self, title, autor):
+        self.title = title
+        self.autor = autor
