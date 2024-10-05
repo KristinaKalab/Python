16d986d0b1f318469391b40d6e5f3a70eece5d2b
KristinaKalab <kris48@yandex.ru>
2024-10-05 13:05:46 +0300

    git add -a

--- training/book.py
+++
@@ -1,4 +0,0 @@
-class Book:
-    def __init__(self, title, autor):
-        self.title = title
-        self.autor = autor
--- training/ex1.py
+++
@@ -1,6 +0,0 @@
-from product import Product
-my_product = Product("Яблоко", 50)
-
-print(my_product.get_name())
-print(my_product.get_price())
-print(my_product.get_description())
